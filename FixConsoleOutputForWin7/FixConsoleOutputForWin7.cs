66db76daf72c2eb0a204ead1cab5f74fb0628392
ZIT WANG <mymisting@gmail.com>
2017-05-02 20:41:47 +0800

    上传代码

---
+++ FixConsoleOutputForWin7/FixConsoleOutputForWin7.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace FixConsoleOutputForWin7
+{
+	[ApiVersion(2, 1)]
+	public class FixConsoleOutputForWin7 : TerrariaPlugin
+	{
+		public override string Name => GetType().Name;
+
+		public override Version Version => GetType().Assembly.GetName().Version;
+
+		public override string Author => "MistZZT";
+
+		public FixConsoleOutputForWin7(Main game) : base(game)
+		{
+			Order = int.MinValue;
+		}
+
+		public override void Initialize()
+		{
+			if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+			{
+				return;
+			}
+
+			if (Environment.OSVersion.Version >= new Version(6, 0) && VersionHelper.IsWindows10OrGreater())
+			{
+				WriteLine("检测到系统版本为Windows 10；不需要运行插件。", ConsoleColor.Cyan);
+				return;
+			}
+
+			try
+			{
+				Console.OutputEncoding = Encoding.GetEncoding("GBK");
+				WriteLine("控制台中文显示修复完毕！", ConsoleColor.Cyan);
+			}
+			catch
+			{
+				// ignored
+			}
+		}
+
+		private static void WriteLine(string content, ConsoleColor color)
+		{
+			var clr = Console.ForegroundColor;
+			Console.ForegroundColor = color;
+			Console.WriteLine(content);
+			Console.ForegroundColor = clr;
+		}
+	}
+}
---
+++ FixConsoleOutputForWin7/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FixConsoleOutputForWin7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FixConsoleOutputForWin7")]
+[assembly: AssemblyCopyright("Copyright © MistZZT 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("44808f2f-21bf-404f-8311-ab1a159a138d")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
